d0b09bdb3d2a40c19b0997c15e80310c89daedfd
Aidan Morrison <aidandmorrison@gmail.com>
2023-04-17 17:07:04 +1000

    with good start on simulations, but still untidy

---
+++ Simulations.R
@@ -0,0 +1,222 @@
+library(tidyverse)
+library(patchwork)
+library(gganimate)
+library(magick)
+
+background_power <- 0.25
+peak_power <- 2
+peak_duration <- 320
+day_mins <- 24*60
+central_dinner_start <- 18*60
+dinner_sd <- 320
+
+# Generate a sequence of minutes for a day
+day_minutes <- seq(0, day_mins - 1, by = 1)
+
+# Convert the x-axis to hours of the day
+day_hours <- day_minutes / 60
+
+# Sample 5 displacement values from the normal distribution
+displacements <- rnorm(15, mean = 0, sd = dinner_sd)
+
+# Calculate the peak start time for each household
+household_peak_starts <- central_dinner_start + displacements
+
+# Define a function that calculates the power demand
+power_demand <- function(time, peak_start, peak_duration, background_power, peak_power) {
+  if (time >= peak_start && time <= (peak_start + peak_duration)) {
+    return(peak_power)
+  } else {
+    return(background_power)
+  }
+}
+
+# Calculate the power demand for each household and store it in a long-format dataframe
+household_demands <- lapply(seq_along(household_peak_starts), function(i) {
+  peak_start <- household_peak_starts[i]
+  demands <- sapply(day_minutes, function(t) power_demand(t, peak_start, peak_duration, background_power, peak_power))
+  data.frame(household = i, time_minutes = day_minutes, power_demand = demands)
+})
+
+household_demands_long <- bind_rows(household_demands)
+
+# Convert the x-axis to hours of the day
+household_demands_long$day_hours <- household_demands_long$time_minutes / 60
+# Calculate the total demand for each minute
+total_demand <- household_demands_long %>%
+  group_by(time_minutes) %>%
+  summarize(total_power_demand = sum(power_demand))
+
+# Add a column to identify the plot type: individual or total
+household_demands_long$plot_type <- "Individual"
+total_demand$plot_type <- "Total"
+
+# Combine the individual and total demand data
+combined_demands <- bind_rows(household_demands_long, total_demand)
+
+# Convert the x-axis to hours of the day
+combined_demands$day_hours <- combined_demands$time_minutes / 60
+
+# Create a plot showing the power demand for each household
+individual_demands_plot <- ggplot(household_demands_long, aes(x = day_hours, y = power_demand, color = as.factor(household))) +
+  geom_line() +
+  labs(x = "Hours of the Day",
+       y = "Power Demand (kW)",
+       title = "Power Demand for 5 Households",
+       color = "Household") +
+  theme_minimal()
+
+# Create a plot showing the total power demand
+total_demands_plot <- ggplot(total_demand, aes(x = day_hours, y = total_power_demand)) +
+  geom_line(color = "black") +
+  labs(x = "Hours of the Day",
+       y = "Total Power Demand (kW)",
+       title = "Total Power Demand") +
+  theme_minimal()
+
+# Combine the two plots using patchwork
+combined_plot <- individual_demands_plot / total_demands_plot
+print(combined_plot)
+
+
+###### video --------
+
+generate_households <- function(n_households, background_power, peak_power, peak_duration, day_mins, central_dinner_start, dinner_sd) {
+  
+  # Sample displacement values from the normal distribution
+  displacements <- rnorm(n_households, mean = 0, sd = dinner_sd)
+  
+  # Calculate the peak start time for each household
+  household_peak_starts <- central_dinner_start + displacements
+  
+  # Generate a sequence of minutes for a day
+  day_minutes <- seq(0, day_mins - 1, by = 1)
+  
+  # Calculate the power demand for each household and store it in a long-format dataframe
+  household_demands <- lapply(seq_along(household_peak_starts), function(i) {
+    peak_start <- household_peak_starts[i]
+    demands <- sapply(day_minutes, function(t) power_demand(t, peak_start, peak_duration, background_power, peak_power))
+    data.frame(household = i, time_minutes = day_minutes, power_demand = demands)
+  })
+  
+  # Combine household demands into a single dataframe
+  household_demands_df <- bind_rows(household_demands, .id = "household")
+  household_demands_df$household <- as.integer(household_demands_df$household)
+  
+  return(household_demands_df)
+}
+
+# Create a function to calculate the total power demand for multiple households
+calculate_total_demand <- function(individual_demands) {
+  total_demands <- individual_demands %>%
+    group_by(day_minutes) %>%
+    summarize(total_power_demand = sum(power_demand)) %>%
+    mutate(day_hours = day_minutes / 60)
+  return(total_demands)
+}
+
+start_value <- 1
+max_value <- 20000
+
+# Calculate the length of the vector
+vector_length <- ceiling(log2(max_value / start_value)) + 1
+
+# Generate the vector
+steps <- sapply(0:(vector_length - 1), function(i) start_value * 2^i)
+
+# Generate the individual power demands for a total of 125 households
+all_individual_demands <- generate_households(steps %>% tail(1), background_power, peak_power, peak_duration, day_mins, central_dinner_start, dinner_sd)
+
+# Initialize an empty dataframe to store the total demands for each step
+all_total_demands <- data.frame()
+demands_summary <- data.frame()
+
+# Loop through the steps
+for (step in steps) {
+  # Filter all_individual_demands to include only households of that step number or lower
+  filtered_demands <- all_individual_demands %>% filter(household <= step)
+  
+  # Summarize total_power_demand by grouping only by time_minutes
+  total_demands_step <- filtered_demands %>%
+    group_by(time_minutes) %>%
+    summarize(total_power_demand = sum(power_demand)) %>%
+    mutate(group = step,
+           day_hours = time_minutes / 60)
+  
+  step_summary <- total_demands_step %>% 
+    summarise(max_power = max(total_power_demand),
+              base_load = min(total_power_demand)) %>% 
+    mutate(group = step,
+           peak_component = max_power - base_load,
+           aggregation_dividend = (step * peak_power) - base_load - peak_component)
+  
+  # Bind the resultant dataframe to all_total_demands
+  all_total_demands <- bind_rows(all_total_demands, total_demands_step)
+  demands_summary <- bind_rows(demands_summary, step_summary)
+}
+demands_summary_long <- demands_summary %>% 
+  select(-max_power) %>% 
+  # select(-aggregation_dividend) %>% 
+  gather(key = "key", value = "value", -group) %>% 
+  mutate(dummyx = "Power")
+
+demands_summary_long %>% 
+  ggplot(aes(x = as.factor(group), y = value, fill = key))+
+  geom_bar(position = "fill", stat = "identity")
+
+# Create the animated plot
+demand_summary_plot <- demands_summary_long %>% 
+  ggplot(aes(fill = key, y = value, x = as.factor(dummyx))) +
+  geom_bar(position = "stack", stat = "identity")+
+  labs(y = "Power Demand (kW)",
+       title = "Demand Summary for {closest_state} Households") +
+  theme_minimal() +
+  transition_states(group, transition_length = 4, state_length = 1) +
+  view_follow(fixed_x = TRUE)
+animate(demand_summary_plot, renderer = gifski_renderer(), fps = 20, duration = 5)
+
+# Create the animated plot
+followed_demands_animation <- ggplot(all_total_demands, aes(x = day_hours, y = total_power_demand, group = group)) +
+  geom_line(color = "black") +
+  labs(x = "Hours of the Day",
+       y = "Total Power Demand (kW)",
+       title = "Total Power Demand for {closest_state} Households") +
+  theme_minimal() +
+  transition_states(group, transition_length = 4, state_length = 1) +
+  view_follow()
+
+unfollowed_demands_animation <- ggplot(all_total_demands, aes(x = day_hours, y = total_power_demand, group = group)) +
+  geom_line(color = "black") +
+  labs(x = "Hours of the Day",
+       y = "Total Power Demand (kW)",
+       title = "Total Power Demand for {closest_state} Households") +
+  theme_minimal() +
+  transition_states(group, transition_length = 4, state_length = 1) 
+
+# Save and view the animation at 4 frames per second
+followed <- followed_demands_animation %>% 
+  animate(renderer = gifski_renderer(), fps = 20, duration = 5)
+unfollowed <- unfollowed_demands_animation %>% 
+  animate(renderer = gifski_renderer(), fps = 20, duration = 5)
+
+a_mgif <- image_read(followed)
+b_mgif <- image_read(unfollowed)
+
+new_gif <- image_append(c(a_mgif[1], b_mgif[1]))
+for(i in 2:15){
+  combined <- image_append(c(a_mgif[i], b_mgif[i]))
+  new_gif <- c(new_gif, combined)
+}
+
+new_gif
+
+
+anim_save("total_demands_animation.gif", total_demands_animation)
+
+
+
+### Delete the plots
+# List all files in the working directory
+files <- list.files(pattern = "gganim_plot")
+# Remove files with 'gganim_plot' in the filename
+file.remove(files)
--- WindScratch.R
+++ WindScratch.R
@@ -63,7 +63,7 @@ plot_correlation(day_summ, 5)
 #### linear model correlation investigation -----
 
 # Compute the correlation matrix for nday_summ with n = 1
-nday_summ <- create_nday_summ(day_summ, 5)
+nday_summ <- create_nday_summ(day_summ, 1)
 
 corr_matrix <- get_corr_matrix(nday_summ)
 
@@ -155,6 +155,145 @@ generate_plots(c("Lincoln Gap", "Port Augusta"), day_summ, long_df)
 generate_plots(c("Rye Park", "Bango 973"), day_summ, long_df)
 
 
+##### PCA ------
+
+wide_data <- nday_summ %>% 
+  pivot_wider(names_from = location, values_from = value) %>% 
+  ungroup() %>% 
+  select(-Period)
 
 
+# Perform PCA
+pca_results <- prcomp(wide_data, center = TRUE, scale. = TRUE)
+
+# Print summary of PCA results
+summary(pca_results)
+
+# Calculate the proportion of variance explained by each principal component
+explained_variance_ratio <- pca_results$sdev^2 / sum(pca_results$sdev^2)
+
+# Print the explained variance ratio
+explained_variance_ratio
+
+# Visualize the explained variance ratio in a scree plot
+
+explained_variance_df <- data.frame(Principal_Component = 1:length(explained_variance_ratio),
+                                    Explained_Variance_Ratio = explained_variance_ratio)
+
+scree_plot <- ggplot(explained_variance_df, aes(x = Principal_Component, y = Explained_Variance_Ratio)) +
+  geom_point() +
+  geom_line() +
+  labs(x = "Principal Component",
+       y = "Explained Variance Ratio",
+       title = "Scree Plot of PCA Explained Variance") +
+  theme_minimal()
+
+print(scree_plot)
+
+# Get the loadings
+loadings <- pca_results$rotation
+
+# Print the loadings for the first few principal components
+loadings[, 1:3]
+
+# Well that didn't exactly work very well. 
+
+##### Fourier -----
+# Step 1: Convert long_df to wide format
+wide_df <- long_df %>%
+  mutate(Datetime = lubridate::ymd_hm(paste(Year, Month, Day, (half_hour - 1) %/% 2, (half_hour - 1) %% 2 * 30, sep = "-"), tz = "UTC")) %>%
+  select(Datetime, location, value) %>%
+  pivot_wider(names_from = location, values_from = value)
+
+# Remove the 'Datetime' column and store the location names
+location_names <- colnames(wide_df)[-1]
+wide_df <- wide_df %>% select(-Datetime)
+
+# Step 2: Calculate the magnitude of the Fourier transform for each location
+fourier_magnitudes <- lapply(wide_df, function(x) {
+  ft <- fft(as.numeric(x), inverse = FALSE)
+  return(Mod(ft)) # Compute the magnitude of the Fourier transform
+})
+
+# Convert the list to a data frame
+fourier_magnitudes_df <- as.data.frame(fourier_magnitudes)
+colnames(fourier_magnitudes_df) <- location_names
+
+# Step 3: Identify the dominant frequency components for each location
+# You can analyze the Fourier magnitudes data frame (fourier_magnitudes_df) to find dominant frequency components for each location
+
+plot_psd <- function(long_df, location_name) {
+  # Filter the data for the specified location
+  location_data <- long_df %>% filter(location == location_name)
+  
+  # Convert the filtered data to a time series
+  time_series <- ts(location_data$value, frequency = 48)
+  
+  # Subtract the mean from the time series
+  centered_time_series <- time_series - mean(time_series)
+  
+  # Calculate the Fourier Transform
+  fourier <- fft(centered_time_series)
+  
+  # Calculate the Power Spectral Density (PSD)
+  psd <- (abs(fourier)^2) / length(centered_time_series)
+  
+  # Create a data frame with the frequencies and PSD values
+  psd_df <- tibble(
+    frequency = (0:(length(psd) - 1)) / length(centered_time_series),
+    psd = psd
+  )
+  
+  # Create the PSD plot
+  plot <- ggplot(psd_df, aes(x = frequency, y = psd)) +
+    geom_line() +
+    labs(x = "Frequency", y = "Power Spectral Density", title = paste("PSD for", location_name)) +
+    theme_minimal() +
+    xlim(0, 0.5) # Limit the x-axis to display only up to the Nyquist frequency
+  
+  # Return the plot
+  return(plot)
+}
+
+
+plot_psd_period <- function(psd, location) {
+  psd_df <- data.frame(frequency = psd$freq, spec = psd$spec)
+  psd_df <- psd_df %>% mutate(period = 1 / frequency) # Calculate the period
+  
+  plot <- ggplot(psd_df, aes(x = period, y = spec)) +
+    geom_line() +
+    labs(x = "Period (hours)",
+         y = "Power Spectral Density",
+         title = paste("PSD for", location)) +
+    theme_minimal() +
+    scale_x_continuous(breaks = seq(0, 48, by = 1)) + # adjust breaks as needed
+    coord_cartesian(xlim = c(0.5, 24 * 7)) # Limit the x-axis to show periods between 0.5 hours and 7 days
+  
+  return(plot)
+}
+
+calculate_psd <- function(long_df, location) {
+  # Filter the long_df dataframe for the desired location
+  filtered_long_df <- long_df %>% filter(location == location)
+  
+  # Convert the dataframe to a time series
+  ts_data <- filtered_long_df %>% 
+    arrange(Year, Month, Day, half_hour) %>% 
+    pull(value) %>% 
+    ts(frequency = 48) # half-hourly data has a frequency of 48
+  
+  # Remove the mean from the time series
+  ts_data <- ts_data - mean(ts_data)
+  
+  # Calculate the power spectral density
+  psd <- spec.pgram(ts_data, taper = 0, log = "no", spans = NULL, fast = TRUE)
+  
+  return(psd)
+}
+
+
+location <- "Lincoln Gap"
+psd <- calculate_psd(long_df, location)
+plot <- plot_psd_period(psd, location)
+print(plot)
 
